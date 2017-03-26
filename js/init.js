4c4675fea1f5498504891c301fddf6d14714fc0f
Wlavell2016 <william.lavell@gmail.com>
2017-03-26 22:59:34 +0700

    finished basic layout

--- js/init.js
+++ js/init.js
@@ -1,3 +1,11 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 900);
+});
+
 (function($) {
 
     $.fn.parallax = function(options) {
